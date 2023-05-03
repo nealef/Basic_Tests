0cc7ece7621450448690dda4b97b8bb149c5406c
Neale Ferguson <neale@sinenomine.net>
2023-05-03 10:29:42 +1000

    Initial Basic_Tests repo

---
+++ Basic_Tests/MeadowApp.cs
@@ -0,0 +1,58 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System;
+using System.Threading.Tasks;
+
+#pragma warning disable 1998
+
+namespace MeadowApp
+{ 
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        public override async Task Initialize()
+        {
+            
+        }
+
+        async Task WillSleep()
+        {
+
+        }
+
+        async Task OnWake()
+        {
+
+        }
+
+        public async override void OnReset()
+        {
+
+        }
+
+        public override async Task Run()
+        {
+            string[] args = { "--verbose" };
+            TestDriver.RunTests (typeof (BasicTests), null);
+            TestDriver.RunTests(typeof(FloatTests), null);
+            TestDriver.RunTests(typeof(LongTests), null);
+            TestDriver.RunTests(typeof(MathsTests), null);
+            TestDriver.RunTests(typeof(CallsTests), null);
+            TestDriver.RunTests(typeof(ArrayTests), null);
+            TestDriver.RunTests(typeof(GenericsTests), null);
+            TestDriver.RunTests(typeof(ExceptionTests), null);
+            TestDriver.RunTests(typeof(ObjectTests), null);
+            TestDriver.RunTests(typeof(VectorTests), null);
+            TestDriver.RunTests(typeof(RATests), null);
+            TestDriver.RunTests(typeof(GSharedTests), null);
+            TestDriver.RunTests(typeof(GCTests), null);
+            TestDriver.RunTests(typeof(DevirtualizationTests), null);
+            // TestDriver.RunTests(typeof(BenchTests), args);
+            Console.WriteLine("**** Tests Complete ****");
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
